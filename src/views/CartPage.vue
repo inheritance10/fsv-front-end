183d890d5c419d94da35f459100710beb0f770b3
Ali cebeci <103434459+inheritance10@users.noreply.github.com>
2024-01-08 03:37:52 +0300

    first update

---
+++ src/views/CartPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="cart">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+</template>
+
+
+<script>
+   export default {
+    name: 'CartPage',
+    props: {
+      msg: String
+    }
+</script>
+
+
+<style>
+
+
+</style>
---
+++ src/views/ProductPage.vue
@@ -0,0 +1,33 @@
+<template>
+
+   <h1>
+    Product Page
+   </h1>
+</template>
+
+
+<script>
+  export default {
+    name: 'ProductPage',
+    props: {
+      msg: String
+    }
+  }
+</script>
+
+<style scoped>
+  h1 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+</style>
